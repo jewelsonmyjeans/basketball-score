a90be2b460139f89cb0c09fb6948e7220cc97285
Aviad Churaman <aviadchuraman@gmail.com>
2022-08-03 21:23:01 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 let homeScore = document.getElementById("home-score")
 let visitorScore = document.getElementById("visitor-score")
 
-function incrementOne() {
-    homeScore.textContent++ 
+
+function incrementHomeOne() {
+    homeScore.textContent++
 }
-function incrementTwo() {
+function incrementHomeTwo() {
     homeScore.textContent = parseInt(homeScore.textContent) + 2
 }
-function incrementThree() {
+function incrementHomeThree() {
+    homeScore.textContent = parseInt(homeScore.textContent) + 3
+}
+function incrementVisitorOne() {
+    visitorScore.textContent++
+}
+function incrementVisitorTwo() {
+    visitorScore.textContent = parseInt(visitorScore.textContent) + 2
+}
+function incrementVisitorThree() {
+    visitorScore.textContent = parseInt(visitorScore.textContent) + 3
 }
