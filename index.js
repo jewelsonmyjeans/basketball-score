f5eed7a9e87ce3ce6d52fb3bdfe67538e2f03b5e
Aviad Churaman <aviadchuraman@gmail.com>
2022-08-03 21:04:22 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+let homeScore = document.getElementById("home-score")
+let visitorScore = document.getElementById("visitor-score")
+
+function incrementOne() {
+    homeScore.textContent++ 
+}
+function incrementTwo() {
+    homeScore.textContent = parseInt(homeScore.textContent) + 2
+}
+function incrementThree() {
+}
